15e4f003de577a17b7770cf52cabc17ec5a1ee86
danis267 <danisgadkari50@email.com>
2022-10-31 17:49:24 -0700

    p file updated

--- app/src/main/java/com/example/alarmapp/PreferenceManager.java
+++ app/src/main/java/com/example/alarmapp/PreferenceManager.java
@@ -1,2 +1,22 @@
-package com.example.alarmapp;public class PreferenceManager {
+package com.example.alarmapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceManager {
+    private final SharedPreferences sharedPreferences;
+
+    public PreferenceManager(Context context){
+        sharedPreferences = context.getSharedPreferences("SharedPreference", Context.MODE_PRIVATE);
+    }
+
+    public void putString(String key, String value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+    public String getString(String key) {
+        return sharedPreferences.getString(key, null);
+    }
+
 }
